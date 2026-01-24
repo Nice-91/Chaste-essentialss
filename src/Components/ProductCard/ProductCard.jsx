5ff22e5d71ff24d4b1afb6496ae58a1e8d0de14a
Nice-91 <iradukundanice91@gmail.com>
2026-01-24 04:12:18 +0200

    updating productcard

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -1,16 +1,18 @@
 import "./ProductCard.css";
 
 const ProductCard = ({ product }) => {
+  console.log("PRODUCT IMAGE:", product.image);
+
   return (
     <div className="card">
-      <img
-        src={product.image || "/placeholder.png"}
-        alt={product.name}
-        className="image"
-      />
+     <img
+  src={product.image ? product.image : "/placeholder.png"}
+  alt={product.name}
+  className="image"
+/>
+
 
       <h3>{product.name}</h3>
-
       <p className="price">RWF {product.price}</p>
 
       {product.description && (
