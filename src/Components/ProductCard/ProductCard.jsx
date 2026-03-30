00a9895c4acdf5a09298d1f92bd62e45fedace2a
Nice-91 <iradukundanice91@gmail.com>
2026-03-30 10:38:18 +0200

    changing whtsp number

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -29,7 +29,7 @@ const ProductCard = ({ product }) => {
       )}
 
       <a
-        href={`https://wa.me/250788747861?text=${encodeURIComponent(
+        href={`https://wa.me/2507887444606?text=${encodeURIComponent(
           `Hello, I want to order ${product.name} from Chaste Essentials`
         )}`}
         className="whatsappButton"
